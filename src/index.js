b69b479dda6582d825231debea23f9017c32dd24
kmsheng <kmsh3ng@gmail.com>
2016-09-21 23:29:29 +0800

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App/App';
 import './index.css';
+import store from './store';
 
-ReactDOM.render(
+const render = () => ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+render();
+store.subscribe(render);
