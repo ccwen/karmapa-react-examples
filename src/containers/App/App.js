33a385293033463065afe3d0f80ac1df9162605b
kmsheng <kmsh3ng@gmail.com>
2016-09-21 23:28:25 +0800

    update App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import Counter from './../../components/Counter/Counter';
+import store from './../../store';
 
 export default class App extends Component {
+
   render() {
+
+    const counterProps = {
+      value: store.getState(),
+      onIncrement: () => store.dispatch({type: 'INCREMENT'}),
+      onDecrement: () => store.dispatch({type: 'DECREMENT'})
+    };
+
+    console.log('here', store.getState());
+
     return (
       <div className="App">
         <div className="App-header">
@@ -12,7 +24,7 @@ export default class App extends Component {
           <h2>Simple Counter</h2>
         </div>
         <div>
-          <Counter />
+          <Counter {...counterProps} />
         </div>
       </div>
     );
