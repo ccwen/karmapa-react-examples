10bb1994535e8b27fad44e1ec3896a1b6e35aa16
kmsheng <kmsh3ng@gmail.com>
2016-09-23 12:03:55 +0800

    add reducer minesweeper

---
+++ src/reducers/minesweeper.js
@@ -0,0 +1,22 @@
+const SET_FACE = 'simple-counter/minesweeper/SET_FACE';
+
+const initialState = {
+  face: '^_^'
+};
+
+export default (state = initialState, action) => {
+
+  switch (action.type) {
+    case SET_FACE:
+      return Object.assign({}, state, {face: action.face});
+    default:
+      return state;
+  }
+}
+
+export function setFace(face) {
+  return {
+    type: SET_FACE,
+    face
+  };
+}
