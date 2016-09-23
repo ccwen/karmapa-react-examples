d7ab39491df97e3e15a1b8c8433908f4ed642f5c
kmsheng <kmsh3ng@gmail.com>
2016-09-23 11:48:04 +0800

    add component MinsweeperCell

---
+++ src/components/MinesweeperCell/MinesweeperCell.js
@@ -0,0 +1,45 @@
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+
+import './MinesweeperCell.css';
+
+export default class MinesweeperCell extends Component {
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    onContextMenu: PropTypes.func.isRequired
+  };
+
+  state = {
+    number: '',
+    isClicked: false,
+    isFlagged: false,
+    showMine: false
+  };
+
+  setClicked = (isClicked) => this.setState({isClicked});
+
+  setNumber = (number) => this.setState({number});
+
+  showMine = (showMine) => this.setState({showMine});
+
+  isMine = () => this.state.isMine;
+
+  isClicked = () => this.state.isClicked;
+
+  setFlagged = (isFlagged) => this.setState({isFlagged});
+
+  render() {
+    const {onClick, onContextMenu} = this.props;
+    const {isClicked, showMine, number, isFlagged} = this.state;
+
+    const tdClass = classNames({
+      'cell': true,
+      'is-mine': showMine,
+      'clicked': isClicked,
+      'flagged': isFlagged,
+      [`c${number}`]: true
+    });
+    return <td className={tdClass} onClick={onClick} onContextMenu={onContextMenu}>{number}</td>
+  }
+}
