ee3e6de620ac5fcbb7e6bffc98c7edb21d4e3a6f
kmsheng <kmsh3ng@gmail.com>
2016-09-23 12:04:14 +0800

    add minesweeper reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import counter from './counter';
+import minesweeper from './minesweeper';
 
 const rootReducer = combineReducers({
-  counter
+  counter,
+  minesweeper
 });
 
 export default rootReducer;
