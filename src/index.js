b479e67cc8fe2e02ca6405061590e3ff4793c546
kmsheng <kmsh3ng@gmail.com>
2016-09-28 22:33:26 +0800

    add middleware storageSyncer

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import {Provider} from 'react-redux';
 import App from './containers/App/App';
 import './index.css';
 import reducer from './reducers/';
+import storageSyncer from './middleware/storageSyncer';
 import logger from './middleware/logger';
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(logger, storageSyncer));
 
 render(
   <Provider store={store}>
