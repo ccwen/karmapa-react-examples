69a9c7f0037673649023b996aa2eeec441c0d36a
kmsheng <kmsh3ng@gmail.com>
2016-09-28 21:35:03 +0800

    remove counter related stuff

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,9 +3,6 @@ import {connect} from 'react-redux';
 
 import logo from './logo.svg';
 import './App.css';
-import Counter from './../../components/Counter/Counter';
-import MinesweeperContainer from './../MinesweeperContainer/MinesweeperContainer';
-import {increment, decrement} from './../../reducers/counter';
 
 class App extends Component {
 
@@ -17,31 +14,15 @@ class App extends Component {
 
   render() {
 
-    const {value, increment, decrement} = this.props;
-
-    const counterProps = {
-      value,
-      onIncrement: increment,
-      onDecrement: decrement
-    };
-
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Simple Counter</h2>
-        </div>
-        <div>
-          <Counter {...counterProps} />
-          <MinesweeperContainer id="m1" width={8} height={8} minesCount={10} />
-          <MinesweeperContainer id="m2" width={16} height={16} minesCount={40} />
-          <MinesweeperContainer id="m3" width={30} height={16} minesCount={99} />
+          <h2>Simple Spreadsheet</h2>
         </div>
       </div>
     );
   }
 }
 
-export default connect(state => ({
-  value: state.counter
-}), {increment, decrement})(App);
+export default connect(state => ({}))(App);
